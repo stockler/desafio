e15cd1ea4f4ca69160563c5cda7aa1259408811c
Rafael Stockler <rafael.stockler@gmail.com>
2016-07-02 22:30:19 -0300

    Fix protocol

--- app/shots/shots.js
+++ app/shots/shots.js
@@ -7,7 +7,7 @@
     var Shots = {};
 
     Shots.popular = function() {
-    	return $http.jsonp('http://api.dribbble.com/v1/shots?access_token=137ef56c6219971a9e471892ca3ba330467298294886bf2519bc4d602c3b4a98&callback=JSON_CALLBACK');
+    	return $http.jsonp('//api.dribbble.com/v1/shots?access_token=137ef56c6219971a9e471892ca3ba330467298294886bf2519bc4d602c3b4a98&callback=JSON_CALLBACK');
     };
 
     return Shots;
--- test/shots/shots.spec.js
+++ test/shots/shots.spec.js
@@ -10,7 +10,7 @@ describe('Shots factory', function() {
 
     $httpBackend = _$httpBackend_;
 
-    $httpBackend.when('JSONP', 'http://api.dribbble.com/v1/shots?access_token=137ef56c6219971a9e471892ca3ba330467298294886bf2519bc4d602c3b4a98&callback=JSON_CALLBACK').respond(defaultJSON.fakeData);
+    $httpBackend.when('JSONP', '//api.dribbble.com/v1/shots?access_token=137ef56c6219971a9e471892ca3ba330467298294886bf2519bc4d602c3b4a98&callback=JSON_CALLBACK').respond(defaultJSON.fakeData);
   }));
 
   it('should exist', function() {
